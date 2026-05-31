f8b5560141af1868d6cf9baf1655e0fc881c4318
Harold <mokamharoldroger@gmail.com>
2026-06-01 00:34:51 +0200

    Début de la créarion des mappers

---
+++ src/main/java/org/harold/plateforme/mapper/PromotionMapper.java
@@ -0,0 +1,57 @@
+package org.harold.plateforme.mapper;
+
+import org.harold.plateforme.dto.promotion.PromotionCreateDTO;
+import org.harold.plateforme.dto.promotion.PromotionDTO;
+import org.harold.plateforme.dto.promotion.PromotionUpdateDTO;
+import org.harold.plateforme.entity.Promotion;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Mapper pour la conversion entre l'entité Promotion et ses DTOs.
+ *
+ * @author Harold
+ * @version 1.0
+ */
+@Component
+public class PromotionMapper {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    /**
+     * Convertit une entité Promotion en PromotionDTO.
+     *
+     * @param promotion     l'entité à convertir
+     * @param nbEtudiants   le nombre d'étudiants actifs dans la promotion
+     * @return              le DTO correspondant
+     */
+    public PromotionDTO toDTO(Promotion promotion, Integer nbEtudiants) {
+        PromotionDTO dto = modelMapper.map(promotion, PromotionDTO.class);
+        dto.setNbEtudiants(nbEtudiants);
+        return dto;
+    }
+
+    /**
+     * Convertit un PromotionCreateDTO en entité Promotion.
+     *
+     * @param dto   le DTO de création
+     * @return      la nouvelle entité Promotion
+     */
+    public Promotion toEntity(PromotionCreateDTO dto) {
+        return modelMapper.map(dto, Promotion.class);
+    }
+
+    /**
+     * Met à jour une entité Promotion existante depuis un PromotionUpdateDTO.
+     *
+     * @param dto           le DTO de modification
+     * @param promotion     l'entité existante à mettre à jour
+     */
+    public void updateEntity(PromotionUpdateDTO dto, Promotion promotion) {
+        promotion.setNom(dto.getNom());
+        promotion.setFiliere(dto.getFiliere());
+        promotion.setDescription(dto.getDescription());
+    }
+}
